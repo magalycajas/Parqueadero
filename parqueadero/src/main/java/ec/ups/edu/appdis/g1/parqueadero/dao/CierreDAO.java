f6a86963e90f886b6ee9226fcd8e0e72a22dfff4
Andres0Loja <49213222+Andres0Loja@users.noreply.github.com>
2020-11-17 12:32:08 -0500

    Parqueadero

---
+++ parqueadero/src/main/java/ec/ups/edu/appdis/g1/parqueadero/dao/CierreDAO.java
@@ -0,0 +1,15 @@
+package ec.ups.edu.appdis.g1.parqueadero.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import ec.ups.edu.appdis.g1.parqueadero.modelo.CierreDiaHora;
+
+public class CierreDAO {
+	public List<CierreDiaHora> getCierreDia(Date dia){
+		return null;
+	}
+	public boolean contabilizar() {
+		return true;
+	}
+}
