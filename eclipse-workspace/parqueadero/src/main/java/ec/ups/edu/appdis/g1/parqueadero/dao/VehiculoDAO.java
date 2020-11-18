ddf5113949717ae0f336d9b560b044d6d16fdf92
Andres0Loja <aloja619@gmail.com>
2020-11-17 22:04:04 -0500

    VehiculoDAO

--- eclipse-workspace/parqueadero/src/main/java/ec/ups/edu/appdis/g1/parqueadero/dao/VehiculoDAO.java
+++ eclipse-workspace/parqueadero/src/main/java/ec/ups/edu/appdis/g1/parqueadero/dao/VehiculoDAO.java
@@ -1,18 +1,58 @@
 package ec.ups.edu.appdis.g1.parqueadero.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 import ec.ups.edu.appdis.g1.parqueadero.modelo.Vehiculo;
 
+@Stateless
 public class VehiculoDAO {
-	public boolean insert(Vehiculo entity) {
+	@Inject
+	private Connection con;
+
+	public VehiculoDAO() {
+
+	}
+
+	public boolean insert(Vehiculo entity) throws SQLException {
+		String sql = "Insert INTO Vehiculo (placa, marca, color) VALUES(?,?,?)";
+
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, entity.getPlaca());
+		ps.setString(2, entity.getMarca());
+		ps.setString(3, entity.getColor());
+		ps.executeUpdate();
+		ps.close();
 		return true;
 	}
-	public boolean update(Vehiculo entity) {
+
+	public boolean update(Vehiculo entity) throws SQLException {
+		String sql = "Update Vehiculo (marca, color) VALUES(?,?) where placa=" + entity.getPlaca();
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, entity.getMarca());
+		ps.setString(2, entity.getColor());
+		ps.executeUpdate();
+		ps.close();
 		return true;
 	}
-	public Vehiculo read(int id) {
+
+	public Vehiculo read(int id) throws SQLException {
+		String sql = "SELECT * FROM Vehiculo where placa=" + id;
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.executeUpdate();
+		ps.close();
 		return null;
 	}
-	public boolean delete(int id) {
+
+	public boolean delete(int id) throws SQLException {
+		String sql = "DELETE FROM Vehiculo WHERE placa = " + id;
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.executeUpdate();
+		ps.close();
 		return true;
 	}
 }
