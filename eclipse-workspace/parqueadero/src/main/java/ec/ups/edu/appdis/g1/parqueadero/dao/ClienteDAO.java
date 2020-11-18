c32818b40cc3bbff24ac420c5520a8e6f341cc64
Andres0Loja <aloja619@gmail.com>
2020-11-17 20:52:30 -0500

    insertar Cliente

--- eclipse-workspace/parqueadero/src/main/java/ec/ups/edu/appdis/g1/parqueadero/dao/ClienteDAO.java
+++ eclipse-workspace/parqueadero/src/main/java/ec/ups/edu/appdis/g1/parqueadero/dao/ClienteDAO.java
@@ -2,31 +2,45 @@ package ec.ups.edu.appdis.g1.parqueadero.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
-import ec.ups.edu.appdis.g1.parqueadero.modelo.Ticket;
+import ec.ups.edu.appdis.g1.parqueadero.modelo.Cliente;
+
 @Stateless
 
 public class ClienteDAO {
+
 	@Inject
 	private Connection con;
+
 	public ClienteDAO() {
-		
+
 	}
-	public boolean insert(Ticket entity) {
-		String sql="Insert INTO Cliente (dni, email, nombre, tipoDocumento"+"VALUES(?,?,?,?";
-		//PreparedStatement ps=con.preparedStatement(sql);
-		
+
+	public boolean insert(Cliente entity) throws SQLException {
+		String sql = "Insert INTO Cliente (dni, email, nombre, tipoDocumento) VALUES(?,?,?,?)";
+
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, entity.getDni());
+		ps.setString(2, entity.getEmail());
+		ps.setString(3, entity.getNombre());
+		ps.setInt(4, entity.getTipoDocumento());
+		ps.executeUpdate();
+		ps.close();
 		return true;
 	}
-	public boolean update(Ticket entity) {
+
+	public boolean update(Cliente entity) {
 		return true;
 	}
-	public Ticket read(int id) {
+
+	public Cliente read(int id) {
 		return null;
 	}
+
 	public boolean delete(int id) {
 		return true;
 	}
